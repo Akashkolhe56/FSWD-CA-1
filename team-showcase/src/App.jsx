f84a876f4e8db1e9231e7ed3670efa203a55f528
Akashkolhe56 <akashkolhe089@gmail.com>
2025-03-04 14:42:00 +0530

    done

---
+++ team-showcase/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import TeamMemberCard from './components/TeamMemberCard'
+function App() {
+  return (
+
+      <div>
+        <TeamMemberCard/>
+      </div>
+  )
+}
+
+export default App
---
+++ team-showcase/src/components/TeamMemberCard.jsx
@@ -0,0 +1,10 @@
+import React from'react';
+const TeamMemberCard = ({name, title})=>{
+    return(
+        <div>
+            <h1>Team member name</h1>
+            <h1>Job title</h1>
+        </div>
+    )
+}
+export default TeamMemberCard;
